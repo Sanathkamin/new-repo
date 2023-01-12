49133292876271aecb094e69eea01c6ff386cf45
Ragnar <sanathkamin802gmail.com>
2023-01-13 00:00:25 +0530

    12-1-2023

---
+++ java/script.js
@@ -0,0 +1,19 @@
+ function display(){
+    alert("hi");
+
+    display1();
+
+    var x=10;
+    x = "Hello";
+    x = true;
+
+    x = 10;
+    y = 20;
+    console.log(x / y);
+    document.write(x); 
+ }
+
+
+ function display1(){
+    alert("called from display");
+ }
